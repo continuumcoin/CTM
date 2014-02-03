ae600db297626cbe322753dcf3b65b008d931ece
thegreatoldone <cthulhu@theoldone.net>
2014-02-03 08:53:11 -0700

    update

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -27,7 +27,7 @@ public:
         pchMessageStart[1] = 0x03;
         pchMessageStart[2] = 0x04;
         pchMessageStart[3] = 0x05;
-        vAlertPubKey = ParseHex("0470ef16e228d49a635cf8be30018f52f355e4f9d7721e6792cc0ce12835008a1bf4d1d50a9c096fa80af2784b6b050b079563fe11634d8c6401c46d32d96d72a6"); //pubkey for boradcast msg
+        txNew.vout[0].scriptPubKey = CScript() << ParseHex("") << OP_CHECKSIG;
         nDefaultPort = 25535;
         nRPCPort = 25536;
         bnProofOfWorkLimit = CBigNum(~uint256(0) >> 20);
@@ -52,10 +52,10 @@ public:
         
         //// debug print
         hashGenesisBlock = genesis.GetHash();
-        while (hashGenesisBlock > bnProofOfWorkLimit.getuint256()){
-            if (++genesis.nNonce==0) break;
-            hashGenesisBlock = genesis.GetHash();
-        }
+        //while (hashGenesisBlock > bnProofOfWorkLimit.getuint256()){
+        //    if (++genesis.nNonce==0) break;
+        //    hashGenesisBlock = genesis.GetHash();
+        //}
 
         printf("%s\n", hashGenesisBlock.ToString().c_str());
         printf("%s\n", genesis.hashMerkleRoot.ToString().c_str());
